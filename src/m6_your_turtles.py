220e76f8466d5d27626f657507be50f6f53f57f9
Gerardo Santana <santang@rose-hulman.edu>
2018-08-30 13:02:53 -0400

    example

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Gerardo Santana.
 """
 ###############################################################################
-# TODO: 1.
+# done: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# done: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -19,7 +19,48 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #     2. Each rg.SimpleTurtle object draws something
 #          (by moving, using its rg.Pen).  ANYTHING is fine!
 #     3. Each rg.SimpleTurtle moves inside a LOOP.
-#
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('midnight blue', 3)
+blue_turtle.speed = 25  # Fast
+
+size = 100
+
+for k in range(13):
+    blue_turtle.draw_circle(size)
+    blue_turtle.pen_up()
+    blue_turtle.right(90)
+    blue_turtle.forward(20)
+    blue_turtle.left(90)
+    blue_turtle.pen_down()
+    size = size - 7
+
+austin = rg.TurtleWindow()
+
+red_turtle = rg.SimpleTurtle('classic')
+red_turtle.pen = rg.Pen('red', 4)
+red_turtle.speed = 15
+
+size = 200
+
+
+
+for k in range(12):
+    red_turtle.draw_square(size)
+    red_turtle.pen_up()
+    red_turtle.right(45)
+    red_turtle.forward(10)
+    red_turtle.left(45)
+    red_turtle.pen_down()
+    size = size - 7
+
+
+
+
+
 #   Be creative!  Strive for way-cool pictures!  Abstract pictures rule!
 #
 #   If you make syntax (notational) errors, no worries -- get help
